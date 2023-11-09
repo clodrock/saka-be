631b46b30522514e5f4ad47d233ab664f3774d98
clodrock <topakkaya.samet@gmail.com>
2023-11-09 18:10:45 +0300

    08112023- cors http

--- src/main/java/com/clodrock/sakabe/configuration/WebConfig.java
+++ src/main/java/com/clodrock/sakabe/configuration/WebConfig.java
@@ -10,5 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/ws/**").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
     }
 }
